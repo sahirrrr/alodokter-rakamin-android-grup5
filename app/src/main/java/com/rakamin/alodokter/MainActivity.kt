64915f49cf2849b1328bfa0a34f877e90cbed028
Sahir Syatha <sahirsyatha77@gmail.com>
2021-12-07 10:18:34 +0700

    initial commit

---
+++ app/src/main/java/com/rakamin/alodokter/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rakamin.alodokter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
