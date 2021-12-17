d214196f4a7e0596d7bc57a68b458ae32628ad51
anggorobeno <anggoro131098@gmail.com>
2021-12-17 20:11:09 +0700

    Update Detail article UI, change status bar background to match fragment layout

--- app/src/main/java/com/rakamin/alodokter/MainActivity.kt
+++ app/src/main/java/com/rakamin/alodokter/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.rakamin.alodokter
 
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.view.*
+import android.view.WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+import androidx.annotation.RequiresApi
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -12,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
@@ -24,12 +31,55 @@ class MainActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.nav_host_home_fragment)
         navView.setupWithNavController(navController)
+        fun setLightStatusBar(status: Boolean) {
+            if (status){
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                    window?.insetsController?.setSystemBarsAppearance(
+                        APPEARANCE_LIGHT_STATUS_BARS,
+                        APPEARANCE_LIGHT_STATUS_BARS
+                    )
+                }
+            }
+            else {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                    window?.insetsController?.setSystemBarsAppearance(0, APPEARANCE_LIGHT_STATUS_BARS)
+                }
+            }
+
+        }
+        fun showStatusBar(status: Boolean){
+            if(status) {
+                WindowCompat.setDecorFitsSystemWindows(window, true)
+            }
+            else {
+                WindowCompat.setDecorFitsSystemWindows(window, false)
+            }
+        }
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
-            if(destination.id == R.id.splashFragment || destination.id == R.id.loginFragment || destination.id == R.id.registerFragment ||  destination.id == R.id.onBoardingFragment) {
+
+            if (destination.id == R.id.loginFragment || destination.id == R.id.registerFragment || destination.id == R.id.onBoardingFragment) {
                 navView.visibility = View.GONE
-            } else {
-                navView.visibility = View.VISIBLE
+                window.statusBarColor = Color.TRANSPARENT
+                setLightStatusBar(true)
+            }
+            when (destination.id) {
+                R.id.detailArticleFragment -> {
+                    showStatusBar(false)
+                    setLightStatusBar(false)
+                }
+
+                R.id.splashFragment -> {
+                    navView.visibility = View.GONE
+                    window.statusBarColor = getColor(R.color.primary)
+                    setLightStatusBar(false)
+                }
+                else -> {
+                    navView.visibility = View.VISIBLE
+                    window.statusBarColor = Color.TRANSPARENT
+                    setLightStatusBar(true)
+                    showStatusBar(true)
+                }
             }
         }
     }
